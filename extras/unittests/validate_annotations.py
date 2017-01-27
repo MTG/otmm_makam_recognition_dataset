2596b3fdac0e6d318df0a2dd61aedfdb56a5b27e
Sertan Senturk <contact@sertansenturk.com>
2017-01-27 12:21:58 +0100

    Added pitch file validation to unittests

--- extras/unittests/validate_annotations.py
+++ extras/unittests/validate_annotations.py
@@ -19,20 +19,45 @@ def test_metadata():
     meta_mbids = get_filenames_in_dir('./data', keyword='*.json')[2]
     meta_mbids = set(os.path.splitext(mm)[0] for mm in meta_mbids)
 
+    pitch_mbids = get_filenames_in_dir('./data', keyword='*.json')[2]
+    pitch_mbids = set(os.path.splitext(mm)[0] for mm in pitch_mbids)
+
     missing_meta = anno_mbids - meta_mbids
     if missing_meta:
-        print "Missing these MBIDS the in ./metadata folder. Please add them!"
+        print "Missing MBIDS in the metadata files in ./data folder. " \
+              "Please add them!"
         for mm in missing_meta:
             print '   {}'.format(mm)
 
             assert False, "Mismatch between the MBIDs in annotations.json " \
-                          "and ./metadata folder"
+                          "and the metadata files in ./data folder"
 
     missing_anno = meta_mbids - anno_mbids
     if missing_anno:
-        print "Extra MBIDS in the ./metadata folder. Please remove them!"
+        print "Extra MBIDS in the metadata files in ./data folder. " \
+              "Please remove them!"
         for ma in missing_anno:
             print '   {}'.format(ma)
 
             assert False, "Mismatch between the MBIDs in annotations.json " \
-                          "and ./metadata folder"
+                          "and the metadata files in ./data folder"
+
+    missing_pitch = anno_mbids - meta_mbids
+    if missing_pitch:
+        print "Missing MBIDS in the pitch files in ./data folder. " \
+              "Please add them!"
+        for mp in missing_pitch:
+            print '   {}'.format(mp)
+
+            assert False, "Mismatch between the MBIDs in annotations.json " \
+                          "and the pitch files in ./data folder"
+
+    missing_anno = missing_pitch - anno_mbids
+    if missing_anno:
+        print "Extra MBIDS in the pitch files in ./data folder. " \
+              "Please remove them!"
+        for ma in missing_anno:
+            print '   {}'.format(ma)
+
+            assert False, "Mismatch between the MBIDs in annotations.json " \
+                          "and the pitch files in ./data folder"
