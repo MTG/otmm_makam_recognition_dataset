539a6823c102acb9b8edf3a2564d66a14d0a055b
Sertan Senturk <contact@sertansenturk.com>
2017-01-27 12:18:31 +0100

    Added basic unittests

---
+++ extras/unittests/validate_annotations.py
@@ -0,0 +1,38 @@
+import json
+import os
+import logging
+from fileoperations.fileoperations import get_filenames_in_dir
+
+logging.basicConfig()  # removes
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def test_metadata():
+    """
+    This test checks if the MBIDs in annotations.json and ./metadata folder
+    are consistent
+    """
+    anno_mbids = json.load(open('./annotations.json'))
+    anno_mbids = set(os.path.split(aa['mbid'])[-1] for aa in anno_mbids)
+
+    meta_mbids = get_filenames_in_dir('./data', keyword='*.json')[2]
+    meta_mbids = set(os.path.splitext(mm)[0] for mm in meta_mbids)
+
+    missing_meta = anno_mbids - meta_mbids
+    if missing_meta:
+        print "Missing these MBIDS the in ./metadata folder. Please add them!"
+        for mm in missing_meta:
+            print '   {}'.format(mm)
+
+            assert False, "Mismatch between the MBIDs in annotations.json " \
+                          "and ./metadata folder"
+
+    missing_anno = meta_mbids - anno_mbids
+    if missing_anno:
+        print "Extra MBIDS in the ./metadata folder. Please remove them!"
+        for ma in missing_anno:
+            print '   {}'.format(ma)
+
+            assert False, "Mismatch between the MBIDs in annotations.json " \
+                          "and ./metadata folder"
